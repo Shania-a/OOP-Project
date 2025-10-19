b867c83bac964a5a3f448d6ee366fa205754881c
Shania-a <shanya.ny@hotmail.se>
2025-10-19 16:10:27 +0200

    Update player.py

--- player.py
+++ player.py
@@ -7,20 +7,16 @@ class Player(ABC):
 
     def __init__(self, piece: str):
         """Create a player with the given piece."""
-        self.set_piece(piece) 
+        self.__piece = piece.upper() 
 
     def get_piece(self) -> str:
         """Return the players piece."""
         return self.__piece
 
-    def set_piece(self, value: str) -> None:
-        """Set the players piece."""
-        self.__piece = value
-
     @abstractmethod
     def make_move(self, board) -> int:
         """
-        Returns a indxed from 0-8 where the player wants to place their piece.
+        Returns an index from 0-8 where the player wants to place their piece.
         Abstract: implemented in subclass.
         """
         pass
